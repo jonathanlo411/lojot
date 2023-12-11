cd26270e5ba62a0dd631212ecf11bd6b5362481f
Jonathan Lo <jonathanlo411@gmail.com>
2023-12-10 17:03:57 -0800

    feat: add general page structure with sample data

---
+++ src/lib/client/subdomain-card.svelte
@@ -0,0 +1,47 @@
+<script lang='ts'>
+    export let name: String;
+    export let title: String;
+    export let description: String | null;
+
+    let redirect = (name: String) => window.open(`https://${name}.lojot.com/`, '_blank')
+</script>
+
+
+<button class="card"
+    on:click={redirect(name)}
+>
+    <h1>{title}</h1>
+    <a href='https://{name}.lojot.com/'>https://{name}.lojot.com/</a>
+</button>
+
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+    * {
+        box-sizing: border-box;
+        margin: 0;
+    }
+    .card {
+		font-family: 'Montserrat', 'Poppins';
+        margin: 0.35rem;
+        border: 1px solid black;
+        border-radius: 7px;
+        background-color: white;
+        padding: 1rem;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        transition: 0.3s;
+    }
+    .card:hover {
+        cursor: pointer;
+        background-color: rgb(241, 241, 241);
+    }
+    .card * {
+        margin: 2%;
+    }
+
+    .card h1 {
+        font-size: 1rem;
+    }
+    a, a:visited { color: blue; }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,97 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang='ts'>
+    import webconfig from "$webconfig/config.json";
+    import PreviewCard from "$lib/client/subdomain-card.svelte";
+
+    let projects: Object[] = webconfig['projects'];
+    let experiments: Object[] = webconfig['experiments'];
+    let schoolwork: Object[] = webconfig['schoolwork'];
+</script>
+
+
+<section>
+    <div id="center">
+        <h1 id="title">Lojot</h1>
+        <p id="description">This site contains information about all the subdomains attached to <a target='_blank' rel='noreferrer' href='https://lojot.com'>lojot.com</a>. The websites list are generally miscellaneous personal projects, schoolwork, things that happen to be attached to my domain.</p>
+        
+        <!-- Projects -->
+        <div class="card-section">
+            <h2>Projects</h2>
+            <div class="card-list">
+            {#each projects as subdomain}
+                <PreviewCard {...subdomain} />
+            {/each}
+            </div>
+        </div>
+
+        <!-- Experiments -->
+        <div class="card-section">
+            <h2>Experiments</h2>
+            <div class="card-list">
+            {#each experiments as subdomain}
+                <PreviewCard {...subdomain} />
+            {/each}
+            </div>
+        </div>
+
+        <!-- Schoolwork -->
+        <div class="card-section">
+            <h2>Schoolwork</h2>
+            <div class="card-list">
+            {#each schoolwork as subdomain}
+                <PreviewCard {...subdomain} />
+            {/each}
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<style global>
+    @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+    * {
+        box-sizing: border-box;
+        margin: 0;
+    }
+    :global(body) {
+        /* General Styling */
+        box-sizing: border-box;
+        margin: 0;
+
+        /* Color vars */
+        --base-bg: #f8f8f8;
+    }
+    a, a:visited { color: blue; }
+    
+    section {
+		font-family: 'Montserrat', 'Poppins';
+		background-color: var(--base-bg);
+		height: 100vh;
+	}
+
+    #center {
+        width: 900px;
+        margin: 0 auto;
+    }
+    #title {
+        padding-top: 3rem;
+        font-size: 2.5rem;
+        font-weight: bold;
+    }
+    #description {
+        font-size: 1.1rem;
+    }
+
+    .card-section {
+        margin: 2rem 0;
+    }
+    .card-section h2 {
+        font-size: 2rem;
+    }
+    .card-list {
+        display: grid;
+        margin: auto;
+        grid-template-columns: repeat(3, 33%);
+        grid-auto-rows: 0.5fr;
+    }
+
+</style>
